2bf23ab990b375980852d1e6bc67eb14e5bc9a4b
Guri9368 <144131390+Guri9368@users.noreply.github.com>
2026-04-04 19:10:06 +0530

    page.tsx updated, layout fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,5 @@
-import LeftSidebar from "@/components/layout/LeftSidebar"
-import RightSidebar from "@/components/layout/RightSidebar"
 import FlowCanvas from "@/components/workflow/FlowCanvas"
 
 export default function Home() {
-  return (
-    <div className="h-screen flex flex-col">
-      
-      
-      <div className="h-14 border-b flex items-center px-6">
-        <h1 className="font-semibold">NextFlow</h1>
-      </div>
-
-      <div className="flex flex-1 overflow-hidden">
-        
-        <LeftSidebar />
-
-        <FlowCanvas />
-
-        <RightSidebar />
-
-      </div>
-    </div>
-  )
+  return <FlowCanvas />
 }
